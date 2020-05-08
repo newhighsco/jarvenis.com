c72e12f8f167d15e415ef2bfe6891054799d79c3
Ben Edwards <hello@benedfit.com>
2020-05-08 11:24:36 +0100

    fix(content-container): reduces padding

--- src/theme/index.js
+++ src/theme/index.js
@@ -1,11 +1,13 @@
 import ContentContainer from '../components/ContentContainer/theme.module.scss'
 import Grid from '../components/Grid/theme.module.scss'
 import CreditLockup from '../components/CreditLockup/theme.module.scss'
+import PageContainer from '../components/PageContainer/theme.module.scss'
 import SiteContainer from '../components/SiteContainer/theme.module.scss'
 
 export default {
   ContentContainer,
   Grid,
   CreditLockup,
+  PageContainer,
   SiteContainer
 }
