7d26a5c66e3e502d26b88496977fa87158f1881c
Ben Edwards <hello@benedfit.com>
2020-05-14 18:08:01 +0100

    fix: adjusts 404 page layout

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -2,20 +2,22 @@ import React from 'react'
 import { object } from 'prop-types'
 import Link from 'next/link'
 import { SmartLink } from '@newhighsco/chipset'
-import { Heading, PageContainer } from '../components'
+import { Heading, PageContainer, Section } from '../components'
 
 const NotFoundPage = ({ meta }) => (
   <PageContainer meta={meta}>
-    <Link href="/" passHref>
-      <SmartLink>
-        <Heading>
-          Page <em>not</em> found
-        </Heading>
-        <Heading as="p" size="small" alternate>
-          Return to homepage
-        </Heading>
-      </SmartLink>
-    </Link>
+    <Section>
+      <Link href="/" passHref>
+        <SmartLink>
+          <Heading>
+            Page <em>not</em> found
+          </Heading>
+          <Heading as="p" size="small" alternate>
+            Return to homepage
+          </Heading>
+        </SmartLink>
+      </Link>
+    </Section>
   </PageContainer>
 )
 
