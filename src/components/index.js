3370e1e2670692161af406f2aa2d67ea37042df8
Ben Edwards <hello@benedfit.com>
2020-07-30 17:33:14 +0100

    fix: exports Card component

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,5 @@
 export { default as BlogListing } from './BlogListing'
+export { default as Card } from './Card'
 export { default as Footer } from './Footer'
 export { default as Header } from './Header'
 export { default as Heading } from './Heading'
