5f22cb9c82d2b7b44e55f8566c3e65e5ae93bc8d
Ben Edwards <hello@benedfit.com>
2020-08-07 20:05:05 +0100

    fix: background colour

--- src/theme/index.js
+++ src/theme/index.js
@@ -3,13 +3,11 @@ import Card from '../components/Card/theme.module.scss'
 import ContentContainer from '../components/ContentContainer/theme.module.scss'
 import Grid from '../components/Grid/theme.module.scss'
 import CreditLockup from '../components/CreditLockup/theme.module.scss'
-import SiteContainer from '../components/SiteContainer/theme.module.scss'
 
 export default {
   Button,
   Card,
   ContentContainer,
   Grid,
-  CreditLockup,
-  SiteContainer
+  CreditLockup
 }
