f12ebdc60c8bf377cebe9923cd19bbb0f4720c8b
Ben Edwards <hello@benedfit.com>
2020-08-07 22:41:51 +0100

    fix: typo

--- next.config.js
+++ next.config.js
@@ -29,7 +29,7 @@ module.exports = withPlugins(
       {
         exclude: /\.svg$/,
         inlineImageLimit: 1,
-        handleImages: ['jpg', 'png', 'ico']
+        handleImages: ['jpeg', 'png', 'ico']
       }
     ],
     [
