19ea8d0a90e6513790544ab93f74b8e6a6c61b4a
Ben Edwards <hello@benedfit.com>
2020-08-08 09:55:46 +0100

    chore: branch specific environment variables

--- next.config.js
+++ next.config.js
@@ -11,12 +11,28 @@ const withSvgr = require('@newhighsco/next-plugin-svgr')
 const withFonts = require('next-fonts')
 const withVideos = require('next-videos')
 
+const branchEnv = branch => {
+  const branches = {
+    master: {
+      SITE_URL: 'https://jarvenis.com',
+      DISALLOW_ROBOTS: false
+    },
+    staging: {
+      SITE_URL: 'https://staging.jarvenis.com',
+      DISALLOW_ROBOTS: true
+    },
+    preview: {
+      SITE_URL: process.env.VERCEL_URL || 'http://localhost:3000',
+      DISALLOW_ROBOTS: true
+    }
+  }
+
+  return branches[branch] || branches.preview
+}
+
 const nextConfig = {
   poweredByHeader: false,
-  env: {
-    SITE_URL: 'https://jarvenis.com/',
-    DISALLOW_ROBOTS: JSON.parse(process.env.DISALLOW_ROBOTS || false)
-  }
+  env: branchEnv(process.env.VERCEL_GITHUB_COMMIT_REF)
 }
 
 sourcebit.fetch(sourcebitConfig, { cache: false })
