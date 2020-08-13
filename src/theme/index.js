d523a92109993363005d65298a53da6e895235c9
Ben Edwards <hello@benedfit.com>
2020-08-13 08:43:18 +0100

    feat(live-stream): themes container
    Closes #148

--- src/theme/index.js
+++ src/theme/index.js
@@ -1,13 +1,15 @@
 import Button from '../components/Button/theme.module.scss'
 import Card from '../components/Card/theme.module.scss'
 import ContentContainer from '../components/ContentContainer/theme.module.scss'
-import Grid from '../components/Grid/theme.module.scss'
 import CreditLockup from '../components/CreditLockup/theme.module.scss'
+import Grid from '../components/Grid/theme.module.scss'
+import LiveStream from '../components/LiveStream/theme.module.scss'
 
 export default {
   Button,
   Card,
   ContentContainer,
+  CreditLockup,
   Grid,
-  CreditLockup
+  LiveStream
 }
