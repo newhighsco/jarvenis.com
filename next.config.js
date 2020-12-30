6dc007e2c578d962843868b97eb2fdeafb7a8507
Ben Edwards <hello@benedfit.com>
2020-12-30 10:15:47 +0000

    chore(sourcebit): disables output

--- next.config.js
+++ next.config.js
@@ -11,7 +11,7 @@ const withVideos = require('next-videos')
 const withMdx = require('next-mdx-frontmatter')
 const envConfig = require('./env.config')
 
-sourcebit.fetch(sourcebitConfig, { cache: false, quiet: false })
+sourcebit.fetch(sourcebitConfig, { cache: false, quiet: true })
 
 const nextConfig = {
   poweredByHeader: false,
