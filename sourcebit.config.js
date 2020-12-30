1e886deaf824ab7432777a19b8739a92a6e22bf1
Ben Edwards <hello@benedfit.com>
2020-12-30 09:42:34 +0000

    chore(sourcebit): disable live updates

--- sourcebit.config.js
+++ sourcebit.config.js
@@ -35,6 +35,7 @@ module.exports = {
     {
       module: require('sourcebit-target-next'),
       options: {
+        liveUpdate: false,
         pages: [
           {
             path: '/{__metadata.urlPath}',
