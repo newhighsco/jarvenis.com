c46d12b8e4bd75724815b136b6b253c7aef42a7a
Ben Edwards <hello@benedfit.com>
2021-02-26 12:28:27 +0000

    feat(header): add webp background image

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -8,7 +8,8 @@ import {
   SmartLink
 } from '@newhighsco/chipset'
 import { LogoLockup } from '..'
-import { links } from '../../data/footer.json'
+import backgroundImage from '../../images/header.jpg?size=1528&format=webp'
+import { links } from '../../data/header.json'
 
 import styles from './Header.module.scss'
 
@@ -20,47 +21,48 @@ const Header = () => {
   }
 
   return (
-    <>
-      <ContentContainer
-        id="top"
-        as="header"
-        role="banner"
-        theme={{ root: styles.root, content: styles.content }}
-        className={classNames(menuVisible && styles.menuVisible)}
-      >
-        <ContentContainer size="desktopLarge" theme={{ content: styles.inner }}>
-          <Grid gutterless flex valign="middle">
-            <Grid.Item className={styles.logo}>
-              <Link href="/" passHref>
-                <SmartLink>
-                  <LogoLockup showText={false} />
-                </SmartLink>
-              </Link>
-            </Grid.Item>
-            <Grid.Item className={styles.navigation}>
-              <Navigation
-                links={links}
-                renderLink={({ href, text, ...rest }) => (
-                  <Link href={href} passHref>
-                    <SmartLink {...rest}>{text}</SmartLink>
-                  </Link>
-                )}
-                theme={{
-                  list: styles.list,
-                  item: styles.item,
-                  link: styles.link,
-                  toggle: styles.toggle,
-                  toggleActive: styles.toggleActive,
-                  toggleIcon: styles.toggleIcon
-                }}
-                toggle
-                onToggle={toggleMenu}
-              />
-            </Grid.Item>
-          </Grid>
-        </ContentContainer>
+    <ContentContainer
+      id="top"
+      as="header"
+      role="banner"
+      theme={{ root: styles.root, content: styles.content }}
+      className={classNames(menuVisible && styles.menuVisible)}
+      style={{
+        backgroundImage: `-webkit-image-set(url(${backgroundImage}) 1x)`
+      }}
+    >
+      <ContentContainer size="desktopLarge" theme={{ content: styles.inner }}>
+        <Grid gutterless flex valign="middle">
+          <Grid.Item className={styles.logo}>
+            <Link href="/" passHref>
+              <SmartLink>
+                <LogoLockup showText={false} />
+              </SmartLink>
+            </Link>
+          </Grid.Item>
+          <Grid.Item className={styles.navigation}>
+            <Navigation
+              links={links}
+              renderLink={({ href, text, ...rest }) => (
+                <Link href={href} passHref>
+                  <SmartLink {...rest}>{text}</SmartLink>
+                </Link>
+              )}
+              theme={{
+                list: styles.list,
+                item: styles.item,
+                link: styles.link,
+                toggle: styles.toggle,
+                toggleActive: styles.toggleActive,
+                toggleIcon: styles.toggleIcon
+              }}
+              toggle
+              onToggle={toggleMenu}
+            />
+          </Grid.Item>
+        </Grid>
       </ContentContainer>
-    </>
+    </ContentContainer>
   )
 }
 
