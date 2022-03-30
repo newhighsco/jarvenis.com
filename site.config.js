29b02d5e21687d0466ac0a2b3b7b01eb5fbee476
Ben Edwards <hello@benedfit.com>
2022-03-30 15:42:44 +0100

    fix: custom Discord link

--- site.config.js
+++ site.config.js
@@ -17,7 +17,7 @@ const config = {
   themeColor: colors.denim,
   twitterHandle: 'jarvenis',
   socialLinks: {
-    discord: 'https://discord.com/invite/QesXtZp',
+    discord: 'https://discord.gg/jarv',
     email: 'jarv@jarvenis.com',
     teespring: 'https://teespring.com/stores/jarv',
     twitch: `https://www.twitch.tv/jarvenis`,
